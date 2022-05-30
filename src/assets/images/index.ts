959b21f779249ce7614fccb1d038554d2a9fc865
Juxhin Bleta <juxhinbleta@gmail.com>
2022-05-31 00:27:38 +0200

    complete hero section

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,5 +1,7 @@
 const infoTimeLine = require("./timeline.png");
+const homeHero = require("./home-hero.png");
 
 export default {
   infoTimeLine,
+  homeHero,
 };
--- src/screens/home/components/Hero.tsx
+++ src/screens/home/components/Hero.tsx
@@ -1,9 +1,18 @@
-type HeroProps = {}
+import assets from "../../../assets";
+import strings from "../../../config/localization/strings";
+
+type HeroProps = {};
 
 function Hero({}: HeroProps) {
   return (
-    <div>Hero</div>
-  )
+    <div className="hero-container">
+      <div className="hero-content">
+        <h2 className="hero-title">{strings.screen.home.hero.title}</h2>
+        <h3 className="hero-subtitle">{strings.screen.home.hero.subtitle}</h3>
+      </div>
+      <img className="hero-bg" src={assets.images.homeHero} alt="" />
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
